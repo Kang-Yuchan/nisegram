8ac7b9ffb1808438fa29565c7e2a379e79cfdf82
Kang.Yuchan <ikimaku0517@gmail.com>
2019-07-05 18:56:30 +0900

    sendMail Function with sendgrid mail

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,21 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env")});
 import { adjectives, nouns } from "./words";
+import sgMail from "@sendgrid/mail";
+
 
 export const generateSecret = () => {
     const randomNumber = Math.floor(Math.random() * adjectives.length);
     return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
 };
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const sendSecretMail = (adress, secret) => ({
+    from: "yuchan@nisegram.com",
+    to: adress,
+    subject: "🔑 Login Secret for Nisegram 🔑",
+    html: `Hey! Your login secret it ${secret}.<br/>Copy paste on the app/website to login`
+})
+sgMail.send(sendSecretMail("rkddbcks123@naver.com", "123"));
