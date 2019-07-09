c433260194bfad785b05f77b7f8052ac3cef1f1b
Kang.Yuchan <ikimaku0517@gmail.com>
2019-07-09 13:30:03 +0900

    adding username check and error

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,6 +4,10 @@ export default {
     Mutation: {
         createAccount: async (_, args) => {
             const { username, email, firstName = "", lastName = "", bio = ""} = args;
+            const exists = await prisma.$exists.user({ username });
+            if (exists) {
+                throw Error("既に存在しているユーザーネームです。");
+            }
             await prisma.createUser({
                 username,
                 email,
